53623d3f412f7101c11bd36c43a1b009f2e8db4b
Suhana664 <mdsuhanasony2002@gmail.com>
2023-12-05 22:52:27 +0530

    second commit

---
+++ style.css
@@ -0,0 +1,55 @@
+*,
+*:before,
+*:after{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Fira Sans", sans-serif;
+    font-size: 18px;
+}
+h1{
+text-align:center;
+font-size:70px;
+font-family:"Fira Sans", sans-serif;
+background-color: #B3446C;
+}
+p{
+text-align:center;
+}
+@media screen and (max-width: 600px) { 
+	.side2 img { 
+		width: 100%; 
+	} 
+}
+body{
+    background-color:#E75480 ;
+}
+.wrapper{
+    width: 450px;
+    background-color: #ffffff;
+    padding: 70px 40px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    left: 50%;
+    top: 50%;
+    box-shadow: 0 20px 25px rgba(0,0,0,0.25);
+    border-radius: 8px;
+    display: flex;
+    justify-content: space-between;
+}
+.container{
+    width: 45%;
+}
+input{
+    width: 100%;
+    height: 50px;
+    border-radius: 5px;
+    border: 2px solid #d2d2d2;
+    outline: none;
+    margin-top: 8px;
+    padding: 0 10px;
+    background-color:#C9A9A6;
+}
+input:focus{
+    border-color: #3164ff;
+}
